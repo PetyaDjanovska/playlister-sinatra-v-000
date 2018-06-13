b056074046fec4a259f8fb535e47627bcee5d667
Petya Djanovska <petya.djanovska@gmail.com>
2018-06-13 14:42:14 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -30,7 +30,7 @@ class SongsController < ApplicationController
   end
 
   patch '/songs/:slug' do
-                    binding.pry
+
     @song = Song.find_by_slug(params[:slug])
     if params['artist_name'].empty?
       @artist = @song.artist
